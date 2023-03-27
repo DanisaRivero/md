ec41693c5dddeed10c5d2f1c19c5a9c066427efb
Danisa <danisarivero@gmail.com>
2023-03-27 20:58:37 -0300

    Nuevo archivo js/00-resumen.js

---
+++ js/00-resumen.js
@@ -0,0 +1,16 @@
+//! LA CONSOLA DE JS
+console.log("Hola");
+console.info("Información");
+console.warn("Advertencia");
+console.error("Error");
+
+//! Presentar por pantalla
+document.write("Hola mundo <br>");
+document.write("Escribimos en la pantalla");
+
+//! PROGRAMAS
+alert("Esto es una alerta");
+
+prompt('Ingresa letras o números');
+
+confirm('¿Deseas continuar?');
